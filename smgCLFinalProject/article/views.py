13850d391935069e46f15ce91d63724017f106af
Boris Angelov <114048646+BorisAngelov23@users.noreply.github.com>
2023-12-24 12:23:17 +0200

    front-end
    news

--- smgCLFinalProject/article/views.py
+++ smgCLFinalProject/article/views.py
@@ -2,6 +2,8 @@ from django.views.generic import ListView, DetailView
 
 from smgCLFinalProject.article.models import Article
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 class ArticleListView(ListView):
     template_name = "article/all_articles.html"
@@ -10,6 +12,22 @@ class ArticleListView(ListView):
 
     def get_queryset(self):
         return Article.objects.all().order_by("-date")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        articles = context['articles']
+
+        paginator = Paginator(articles, 8)
+        page = self.request.GET.get('page')
+
+        try:
+            context['articles'] = paginator.page(page)
+        except PageNotAnInteger:
+            context['articles'] = paginator.page(1)
+        except EmptyPage:
+            context['articles'] = paginator.page(paginator.num_pages)
+
+        return context
 
 
 class ArticleDetailView(DetailView):
